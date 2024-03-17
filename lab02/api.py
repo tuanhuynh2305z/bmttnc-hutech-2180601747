11daa55b712f8b73f5ba2c6b5c6a7f2dab5ef5d5
tuanhuynh2305z <ADMIN@DESKTOP-GHISU8I.com>
2024-03-17 15:15:53 +0700

    [add] lab02 playfair

--- lab02/api.py
+++ lab02/api.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from  cipher.ceasar import CaesarCipher
 from cipher.vigenere.vigenere_cipher import VigenereCipher
 from cipher.railfence import RailFenceCipher
+from cipher.playfair import PlayFairCipher
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
@@ -9,6 +10,8 @@ caesar_cipher = CaesarCipher()
 vigenere_cipher = VigenereCipher()
 
 railfence_cipher = RailFenceCipher()
+
+playfair_cipher = PlayFairCipher()
 
 # @app.route('/registrazione', methods=['GET', 'POST']) 
 @app.route("/api/caesar/encrypt", methods = ["POST"])
@@ -58,6 +61,32 @@ def decrypt():
     key=int(data['key'])
     decrypted_text=railfence_cipher.rail_fence_decrypt(cipher_text,key)
     return jsonify({'decrypted_text': decrypted_text})
+
+@app.route('/api/playfair/creatematrix', methods = ['POST'])
+def playfair_creatematrix():
+    data = request.json
+    key = data ['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    return jsonify({"playfair_matrix": playfair_matrix})
+
+@app.route('/api/playfair/encrypt', methods = ['POST'])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/playfair/decrypt', methods = ['POST'])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text':decrypted_text})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
